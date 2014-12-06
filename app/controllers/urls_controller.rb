67376452c92013e44e56ab36f9312f164dd4c52c
David Rozenberg <daveroze@gmail.com>
2014-12-06 17:31:31 -0500

    add input area for short url and urls#redirect_to action

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -22,12 +22,28 @@ class UrlsController < ApplicationController
     end
   end
 
+  def redirect_to
+    @url = Url.find_by_short_url(url_params[:short_url])
+    if @url
+      @url.num_visits += 1
+      @url.save
+      @urls = Url.order(num_visits: :desc).limit(100)
+      render :index
+    else
+      @urls = Url.order(num_visits: :desc).limit(100)
+      flash[:errors] = ["Invalid Short URL"]
+      render :index
+    end
+
+  end
+
   def redirect
     short_url = params[:short_url]
     url = Url.find_by_short_url(short_url)
     url.num_visits += 1
     url.save
     long_url = url.long_url
+    @urls
     redirect_to long_url
   end
 
@@ -38,7 +54,7 @@ class UrlsController < ApplicationController
   private
 
   def url_params
-    params.require(:url).permit(:long_url)
+    params.require(:url).permit(:long_url, :short_url)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :urls, only: [:index, :create, :destroy]
 
   get 'urls/:short_url', to: 'urls#redirect', as: 'redirect'
+  post 'urls/redirect', to: 'urls#redirect_to', as: 'redirect_to'
 
 end
